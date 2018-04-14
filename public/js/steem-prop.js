9da463826ef38cc3db08882dafb68287d83a8580
tdreid <trevor.d.reid@gmail.com>
2018-04-14 16:48:56 +0000

    Cleanup whitespace

--- public/js/steem-prop.js
+++ public/js/steem-prop.js
@@ -2,11 +2,11 @@
 var ViewModel = function() {
   this.converter = new showdown.Converter();
   this.pad = ko.observable('Write something...');
-  
+
   this.md = ko.computed(function() {
     return this.converter.makeHtml(this.pad());
   }, this);
-  
+
   this.syncScroll = function(d, e) {
     document.getElementById('preview').scrollTop = e.target.scrollTop;
   };
