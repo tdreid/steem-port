363fee04cb8f5b1fb8593569a80ea1fae0ac6418
tdreid <trevor.d.reid@gmail.com>
2018-04-14 00:25:38 +0000

    Make it pretty

--- public/js/steem-prop.js
+++ public/js/steem-prop.js
@@ -1,10 +1,10 @@
 /* global showdown ko */
 var ViewModel = function() {
-    this.converter = new showdown.Converter();
-    this.pad = ko.observable('Write something...');   
-     this.md = ko.computed(function() {
-        return this.converter.makeHtml(this.pad());
-     }, this);
+  this.converter = new showdown.Converter();
+  this.pad = ko.observable('Write something...');
+  this.md = ko.computed(function() {
+    return this.converter.makeHtml(this.pad());
+  }, this);
 };
 
 ko.applyBindings(new ViewModel());
