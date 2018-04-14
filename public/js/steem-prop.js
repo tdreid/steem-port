318c6645974240e859344dc093b322245aa88d21
tdreid <trevor.d.reid@gmail.com>
2018-04-14 16:47:09 +0000

    Bind preview scrolling to editor scrolling

--- public/js/steem-prop.js
+++ public/js/steem-prop.js
@@ -2,9 +2,14 @@
 var ViewModel = function() {
   this.converter = new showdown.Converter();
   this.pad = ko.observable('Write something...');
+  
   this.md = ko.computed(function() {
     return this.converter.makeHtml(this.pad());
   }, this);
+  
+  this.syncScroll = function(d, e) {
+    document.getElementById('preview').scrollTop = e.target.scrollTop;
+  };
 };
 
 ko.applyBindings(new ViewModel());
